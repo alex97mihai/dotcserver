01a34b4c85599c1d25b84793757c7e24ac8468f6
Ubuntu <ubuntu@ip-172-31-3-237.eu-west-1.compute.internal>
2018-03-01 18:49:26 +0000

    elapsed time working in celery task

--- my_app/tasks.py
+++ my_app/tasks.py
@@ -1,14 +1,20 @@
 from celery import task
-from my_app.models import Order
+from my_app.models import Order as OrderList
 from datetime import date, time, datetime
 
 
 @task
 def test_celery():
-    test = Order.objects.all()
-    test_order = datetime.combine(date.min, test[1].time) - datetime.combine(date.min, test[0].time) 
+    timeLimit = 5
+    orders = OrderList.objects.all()
     out = open('orders.txt', 'a')
-    out.write(str(test_order))
+    for order in orders:
+        # check if X minutes have elapsed and then complete due orders
+        now = datetime.now().strftime('%H:%M:%S')
+        now = datetime.strptime(now, '%H:%M:%S').time()
+        elapsedTime = datetime.combine(date.min, now) - datetime.combine(date.min, order.time)
+
+        out.write(str(elapsedTime)+' ')
     out.close
 
 
